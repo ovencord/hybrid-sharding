ff235aeb157148246066f38478f38b1adf2b9c4c
Luigi Colantuono <163913397+LuigiColantuono@users.noreply.github.com>
2026-02-20 09:05:52 +0100

    Adding paths redirect to local .d.ts stub to block node_modules resolution

---
+++ src/types/ovencord-discord.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Minimal type stub for @ovencord/discord.js.
+ * Prevents TypeScript from following into node_modules source files.
+ */
+declare module "@ovencord/discord.js" {
+    export class Client {
+        [key: string]: any;
+    }
+}
